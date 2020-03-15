a03273f50685c773b412fa47f97faaedfb200fd4
hfurkanvural <hfurkanvural@gmail.com>
2020-03-15 16:36:57 +0300

    firs commit

---
+++ Models/Knowledge.cs
@@ -0,0 +1,10 @@
+namespace blazor_app.Models
+{
+    public class Knowledge
+    {
+        public string name { get; set; }
+        public string date { get; set; }
+        public string details {get; set;}
+        public long id { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace blazor_app
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args)
+            //.UseUrls("http://localhost:5050")
+            .Build()
+            .Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
